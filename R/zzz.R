a22ef9b6151df5aac1aefd99e3212816a5d30984
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-03-09 11:04:25 -0700

    Implement CallrFutureBackend based on the new, upcoming FutureBackend API

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,8 @@
 FutureRegistry <- NULL
 assertOwner <- NULL
 
+attr(callr, "backend") <- CallrFutureBackend
+
 .onLoad <- function(libname, pkgname) {
   ## Import private functions from 'future'
   FutureRegistry <<- import_future("FutureRegistry")

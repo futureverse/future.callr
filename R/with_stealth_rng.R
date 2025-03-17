d0570192bd52751bf4f1e353f67941b24fb37806
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-03-17 09:02:19 -0700

    revert to $<- just in case [ci skip]

--- R/with_stealth_rng.R
+++ R/with_stealth_rng.R
@@ -10,7 +10,7 @@ with_stealth_rng <- function(expr, substitute = TRUE, envir = parent.frame(), ..
         rm(list = ".Random.seed", envir = .GlobalEnv, inherits = FALSE)
       }
     } else {
-      .GlobalEnv[[".Random.seed"]] <- oseed
+      .GlobalEnv$.Random.seed <- oseed
     }
   })
 

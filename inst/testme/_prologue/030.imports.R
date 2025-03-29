01f23cda06e01d216a7d54204075e4abde9a44ad
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-03-29 13:09:04 -0700

    TESTS: Move to 'testme' structure

---
+++ inst/testme/_prologue/030.imports.R
@@ -0,0 +1,13 @@
+## Private future.callr functions
+await <- future.callr:::await
+import_future <- future.callr:::import_future
+is_false <- future.callr:::is_false
+is_na <- future.callr:::is_na
+is_os <- future.callr:::is_os
+hpaste <- future.callr:::hpaste
+mcat <- future.callr:::mcat
+mprintf <- future.callr:::mprintf
+mprint <- future.callr:::mprint
+mstr <- future.callr:::mstr
+printf <- future.callr:::printf
+trim <- future.callr:::trim
---
+++ inst/testme/test-callr,worker-termination.R
@@ -0,0 +1,26 @@
+library(future.callr)
+
+message("*** callr() - terminating workers ...")
+
+plan(callr, workers = 2L)
+
+all <- nbrOfWorkers()
+free <- nbrOfFreeWorkers()
+stopifnot(
+  nbrOfWorkers() == 2L,
+  nbrOfFreeWorkers() == 2L
+)
+
+## Force R worker to quit
+f <- future({ tools::pskill(pid = Sys.getpid()) })
+res <- tryCatch(value(f), error = identity)
+print(res)
+stopifnot(inherits(res, "FutureError"))
+
+stopifnot(
+  nbrOfWorkers() == all,
+  nbrOfFreeWorkers() == free
+)
+
+message("*** callr() - terminating workers ... DONE")
+

105de84bf1991aaf551fe4c035fb45709d797518
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-05-14 15:22:20 -0700

    future (>= 1.49.0) is now on CRAN

--- R/001.import_future_functions.R
+++ R/001.import_future_functions.R
@@ -18,32 +18,15 @@ import_future_functions <- function() {
   signalEarly <<- import_future("signalEarly")
   
   ## future (>= 1.40.0)
-  prune_fcn <<- import_future("prune_fcn", default = prune_fcn)
-  evalFuture <<- import_future("evalFuture", default = NULL)
-  getFutureData <<- import_future("getFutureData", default = NULL)
+  prune_fcn <<- import_future("prune_fcn")
+  evalFuture <<- import_future("evalFuture")
+  getFutureData <<- import_future("getFutureData")
   getFutureBackendConfigs <<- import_future("getFutureBackendConfigs")
   registerS3method("getFutureBackendConfigs", "CallrFuture", getFutureBackendConfigs.CallrFuture)
 
-  ## Until future (>= 1.49.0) is on CRAN
-  cancel <<- import_future("cancel", default = NA)
-  if (!is.function(cancel)) {
-    interrupt <- import_future("interrupt")
-    cancel <<- function(x, interrupt = TRUE, ...) {
-      if (!interrupt) return(x)
-      interrupt(x, ...)
-    }
-  }
-
-  ## Until future (>= 1.49.0) is on CRAN
-  sQuoteLabel <<- import_future("sQuoteLabel", default = function(label) {
-    if (is.null(label)) {
-        "NULL"
-    } else if (is.na(label)) {
-        "NA"
-    } else {
-        sQuote(label)
-    }
-  })
+  ## future (>= 1.49.0)
+  cancel <<- import_future("cancel")
+  sQuoteLabel <<- import_future("sQuoteLabel")
 
   .debug <<- import_future(".debug", mode = "environment", default = new.env(parent = emptyenv()))
 }
